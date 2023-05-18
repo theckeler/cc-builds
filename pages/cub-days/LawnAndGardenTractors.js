a161bec59e6996b2ee49dc36ca01ba83c94fe953
Todd Heckeler <todd@consumedesign.com>
2023-05-18 10:42:47 -0400

    mini riders

--- pages/cub-days/LawnAndGardenTractors.js
+++ pages/cub-days/LawnAndGardenTractors.js
@@ -21,10 +21,14 @@ export default function LawnAndGardenTractors() {
 				<div
 					className="position-lg-absolute w-100 h-100"
 					style={{ zIndex: 1, top: 0, left: 0 }}>
-					<div className="container pt-lg-9 bg-black bg-lg-transparant">
-						<div className="p-2 w-lg-500 bg-black">
-							<h2 className="h1">CUB CADET DAYS</h2>
-							<p className="display-4 mb-0">
+					<div className="container pt-lg-9 bg-yellow bg-lg-transparant">
+						<div className="text-lg-white p-2 w-lg-500">
+							<h2 className="h1" style={{ textShadow: "0px 0px 10px black" }}>
+								CUB CADET DAYS
+							</h2>
+							<p
+								className="display-4 mb-0"
+								style={{ textShadow: "0px 0px 10px black" }}>
 								Get $150 off the XT Enduro Series now through June 17th*
 							</p>
 							<small className="mb-0 pl-4" style={{ fontSize: "10px" }}>
