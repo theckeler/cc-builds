6f82b01e85c5d44aa1cdd0ef2738e40915b52ae3
Todd Heckeler <todd@consumedesign.com>
2023-08-23 14:45:51 -0400

    adding sizes to images

--- templates/Header.js
+++ templates/Header.js
@@ -11,6 +11,8 @@ function Header() {
 						className="icon logo"
 						src="https://staging-na01-mtd.demandware.net/on/demandware.static/-/Sites-cubcadet-Library/default/v1d5f49f5a13f1eba170bf9f8624abecd5fbcf8a0/images/for-dev/logo.svg"
 						alt="Cub Cadet"
+						width="180"
+						height="26"
 					/>
 				</Link>
 			</div>
