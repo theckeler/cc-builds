c6a8248fe5cf0d3b9c4a4fd95ca74e331376794d
Todd Heckeler <todd@consumedesign.com>
2023-10-05 11:12:08 -0400

    schema

--- components/Articles.js
+++ components/Articles.js
@@ -1,6 +1,60 @@
 import ImgCopyBlockStacked from "./ImgCopyBlockStacked";
 
 const Articles = ({ articles, className, numRows }) => {
+	// let schemea = {
+	// 	"@context": "https://schema.org",
+	// 	"@type": "NewsArticle",
+	// 	name: "Articles & How To’s",
+	// };
+	// let hasPart = [];
+	// faqs.forEach(function (block) {
+	// 	hasPart = [
+	// 		...hasPart,
+	// 		{
+	// 			"@type": "BackgroundNewsArticle",
+	// 		},
+	// 	];
+	// });
+	// schemea = { ...schemea, hasPart: hasPart };
+
+	{
+		/*
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Periodical",
+  "issn": "0140-6736",
+  "hasPart": {
+    "@id": "vol376",
+    "@type": "PublicationVolume",
+    "volumeNumber": "376",
+    "hasPart": [
+      {
+        "@id": "issue9735",
+        "@type": "PublicationIssue",
+        "datePublished": "2010-07-03",
+        "pageEnd": "140",
+        "pageStart": "69",
+        "issueNumber": "9735"
+      },
+      {
+        "@id": "issue9734",
+        "@type": "PublicationIssue",
+        "datePublished": "2010-07-03",
+        "pageEnd": "68",
+        "pageStart": "1",
+        "issueNumber": "9734"
+      }
+    ]
+  },
+  "name": "The Lancet",
+  "publisher": "Elsevier"
+}
+</script>
+
+		*/
+	}
+
 	return (
 		<section className={className}>
 			<div className="wrapper">
@@ -20,6 +74,13 @@ const Articles = ({ articles, className, numRows }) => {
 				<div className="slick pl-3 pr-3">
 					<ul className="list-unstyled d-flex flex-column flex-md-row flex-wrap">
 						{articles.blocks.map((block, i) => {
+							let schemea = {
+								"@context": "https://schema.org",
+								"@type": "NewsArticle",
+								headline: block.title,
+								image: [block.img],
+							};
+
 							return (
 								<li
 									className={`col-sm-12 col-md-${
@@ -30,6 +91,12 @@ const Articles = ({ articles, className, numRows }) => {
 										{...{ block }}
 										className="p-2 h-100"
 										style={{ backgroundColor: "#efefef" }}
+									/>
+									<script
+										type="application/ld+json"
+										dangerouslySetInnerHTML={{
+											__html: JSON.stringify(schemea),
+										}}
 									/>
 								</li>
 							);
