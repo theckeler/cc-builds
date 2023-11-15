d480f26c30694c826f0d8f2637b1f0675b55feb5
Todd Heckeler <todd@consumedesign.com>
2023-11-15 06:15:13 -0500

    read me

--- components/FinancingCard.js
+++ components/FinancingCard.js
@@ -5,14 +5,14 @@ export default function FinancingCard({ plansData }) {
 				plansData.map((block, i) => {
 					return (
 						<div className="card-container" key={i}>
-							<div className="card">
-								<div className="card-body">
+							<div className="card flex flex-col h-full">
+								<div className="card-body flex flex-col">
 									<div className="front">
 										<h3 className="card-title">
 											{block.term} <span>months</span>
 										</h3>
 									</div>
-									<div className="back">
+									<div className="back h-full flex flex-col">
 										<h4
 											className="card-title"
 											dangerouslySetInnerHTML={{ __html: block.title }}
@@ -21,6 +21,12 @@ export default function FinancingCard({ plansData }) {
 											<p
 												className="card-text"
 												dangerouslySetInnerHTML={{ __html: block.text }}
+											/>
+										)}
+										{!!block.fee && (
+											<p
+												className="card-text font-bold"
+												dangerouslySetInnerHTML={{ __html: block.fee }}
 											/>
 										)}
 										<p
@@ -36,7 +42,7 @@ export default function FinancingCard({ plansData }) {
 											</span>
 										</p>
 										<a
-											className="btn-text"
+											className="btn-text mt-auto"
 											data-target="#financing-modal"
 											data-toggle="modal"
 											href="#"
