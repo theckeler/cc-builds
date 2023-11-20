6c444e0afe000835c6842dd6ad252533d87b9c8c
Todd Heckeler <todd@consumedesign.com>
2023-11-20 08:20:57 -0500

    snow days

--- templates/Header.js
+++ templates/Header.js
@@ -1,23 +1,24 @@
 import Link from "next/link";
 
 function Header() {
-	return (
-		<header>
-			<div
-				className="wrapper ml-auto mr-auto d-flex"
-				style={{ padding: "20px", maxWidth: "1400px" }}>
-				<Link href="/" title="Cub Cadet Home">
-					<img
-						className="icon logo"
-						src="https://staging-na01-mtd.demandware.net/on/demandware.static/-/Sites-cubcadet-Library/default/v1d5f49f5a13f1eba170bf9f8624abecd5fbcf8a0/images/for-dev/logo.svg"
-						alt="Cub Cadet"
-						width="180"
-						height="26"
-					/>
-				</Link>
-			</div>
-		</header>
-	);
+  return (
+    <header>
+      <div
+        className="wrapper ml-auto mr-auto d-flex"
+        style={{ padding: "20px", maxWidth: "1400px" }}
+      >
+        <Link href="/" title="Cub Cadet Home">
+          <img
+            className="icon logo"
+            src="https://staging-na01-mtd.demandware.net/on/demandware.static/-/Sites-cubcadet-Library/default/v1d5f49f5a13f1eba170bf9f8624abecd5fbcf8a0/images/for-dev/logo.svg"
+            alt="Cub Cadet"
+            width="180"
+            height="26"
+          />
+        </Link>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
